020c0a83f0d9d35f991a8ed1c6ba15cde74ebc6e
Luis Fonseca <miguelluisfonseca@gmail.com>
2013-12-16 23:31:27 +0000

    Implemented testing with mongodb integration.

--- EuroMatch/src/pt/up/fe/rvau/euromatch/DetectedBillsResult.java
+++ EuroMatch/src/pt/up/fe/rvau/euromatch/DetectedBillsResult.java
@@ -1,9 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package pt.up.fe.rvau.euromatch;
 
 import java.util.List;
@@ -17,4 +11,5 @@ public class DetectedBillsResult {
 	public List<DetectedBill> detectedBills;
 	public Mat outputImage;
 	public int totalValue;
+    public double elapsedTime;
 }
--- EuroMatch/src/pt/up/fe/rvau/euromatch/EuroMatch.java
+++ EuroMatch/src/pt/up/fe/rvau/euromatch/EuroMatch.java
@@ -1,38 +1,17 @@
 package pt.up.fe.rvau.euromatch;
 
-import com.google.gson.Gson;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.Mongo;
 import java.io.BufferedWriter;
-import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
-import pt.up.fe.rvau.euromatch.jutils.PointUtils;
 import java.io.IOException;
-import java.io.Reader;
-import java.io.StringWriter;
-import java.io.Writer;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
-import org.opencv.calib3d.Calib3d;
-import org.opencv.core.Core;
-import org.opencv.core.CvType;
-import org.opencv.core.Mat;
-import org.opencv.core.MatOfDMatch;
-import org.opencv.core.MatOfKeyPoint;
-import org.opencv.core.MatOfPoint2f;
-import org.opencv.core.Point;
-import org.opencv.core.Scalar;
-import org.opencv.features2d.DMatch;
+import java.net.UnknownHostException;
 import org.opencv.features2d.DescriptorExtractor;
 import org.opencv.features2d.DescriptorMatcher;
 import org.opencv.features2d.FeatureDetector;
-import org.opencv.features2d.Features2d;
-import org.opencv.features2d.KeyPoint;
 import org.opencv.highgui.Highgui;
-import org.opencv.imgproc.Imgproc;
-import pt.up.fe.rvau.euromatch.jutils.ImageConverter;
-import pt.up.fe.rvau.euromatch.jutils.MatConverter;
-import pt.up.fe.rvau.euromatch.jutils.VisualHelper;
 
 /**
  *
@@ -69,49 +48,84 @@ public class EuroMatch {
 		};
 	public static final Integer[] DESCRIPTOR_MATCHERS = new Integer [] {
 			DescriptorMatcher.BRUTEFORCE,
-			DescriptorMatcher.FLANNBASED
+			DescriptorMatcher.FLANNBASED,
+            DescriptorMatcher.BRUTEFORCE_HAMMING
 		};
 	public static final String[] DESCRIPTOR_MATCHERS_STRING = new String [] {
 			"DescriptorMatcher.BRUTEFORCE",
-			"DescriptorMatcher.FLANNBASED"
+			"DescriptorMatcher.FLANNBASED",
+            "DescriptorMatcher.BRUTEFORCE_HAMMING"
 		};
     /**
      * @param args the command line arguments
      */
-    public static void xmain(String[] args) {
-        System.out.println("Starting");
-		
-		String imageFiles[] = new String[] { "scenes.png", "scenesx.png", "scenesy20r.png",  "scenesy50r.png"};
-		
-		for (int d = 0; d < FEATURE_DETECTORS.length; ++d)
-		{
-			for (int e = 0; e < DESCRIPTOR_EXTRACTORS.length; ++e)
-			{
-				for (int m = 0; m < DESCRIPTOR_MATCHERS.length; ++m)
-				{
-					for (String imageFileName : imageFiles)
-					{
-						int featureDetector = FEATURE_DETECTORS[d];
-						int descriptorExtractor = DESCRIPTOR_EXTRACTORS[e];
-						int descriptorMatcher = DESCRIPTOR_MATCHERS[m];
-						String algorithmsCombination =
-								FEATURE_DETECTORS_STRING[d] + "-" +
-								DESCRIPTOR_EXTRACTORS_STRING[e] + "-" +
-								DESCRIPTOR_MATCHERS_STRING[m];
-						try {
-							testPerformDetection(imageFileName, algorithmsCombination, featureDetector, descriptorExtractor, descriptorMatcher);
-						} catch(Exception ex)
-						{
-							//System.out.println("Error: " + ex.getMessage());
-							//ex.printStackTrace();
-						}
-					}
-				}
-			}
-		}
+    public static void main(String[] args) throws UnknownHostException {
+        System.out.println("Starting tests");
+		testSingleNoteResillience();
 	}
+    
+    public static void testSingleNoteResillience() throws UnknownHostException {
+        
+        Mongo mongo = new Mongo();
+        DB db = mongo.getDB("testset");
+        DBCollection dbCollection = db.getCollection("singleNotes");
+        String[] imageFiles = new String[] {
+            "test5.png",
+            "test5r.png",
+            "test10.png", "test10r.png",
+            "test20.png", "test20r.png",
+            "test50.png", "test50r.png"
+        };
+        int[] imageFilesNoteValue = new int[] {
+            5, 5,
+            10, 10,
+            20, 20,
+            50, 50
+        };
+
+        for (int d = 0; d < FEATURE_DETECTORS.length; ++d) {
+            for (int e = 0; e < DESCRIPTOR_EXTRACTORS.length; ++e) {
+                for (int m = 0; m < DESCRIPTOR_MATCHERS.length; ++m) {
+                    for (int i = 0; i < imageFiles.length; ++i) {
+                        String imageFileName = imageFiles[i];
+                        int featureDetector = FEATURE_DETECTORS[d];
+                        int descriptorExtractor = DESCRIPTOR_EXTRACTORS[e];
+                        int descriptorMatcher = DESCRIPTOR_MATCHERS[m];
+                        String algorithmsCombination
+                           = FEATURE_DETECTORS_STRING[d] + "-"
+                           + DESCRIPTOR_EXTRACTORS_STRING[e] + "-"
+                           + DESCRIPTOR_MATCHERS_STRING[m];
+                        try {
+                            DetectedBillsResult result
+                               = testPerformDetection(imageFileName, algorithmsCombination, featureDetector, descriptorExtractor, descriptorMatcher);
+                            
+                            int totalValue = 0;
+                            for (DetectedBill bill : result.detectedBills)
+                            {
+                                totalValue += bill.getValue();
+                            }
+                            
+                            BasicDBObject document = new BasicDBObject("file", imageFiles[i]).
+                                append("featureDetectors", FEATURE_DETECTORS_STRING[d]).
+                                append("descriptorExtractor", DESCRIPTOR_EXTRACTORS_STRING[e]).
+                                append("descriptorMatcher", DESCRIPTOR_MATCHERS_STRING[m]).
+                                append("notesValue", imageFilesNoteValue[i]).
+                                append("notesFoundCount", result.detectedBills.size()).
+                                append("elapsedTime", result.elapsedTime).
+                                append("totalValue", totalValue);
+                            
+                            dbCollection.insert(document);
+                        } catch (Exception ex) {
+                            System.out.println("Error: " + ex.getMessage());
+                            ex.printStackTrace();
+                        }
+                    }
+                }
+            }
+        }
+    }
 	
-	public static void testPerformDetection(
+	public static DetectedBillsResult testPerformDetection(
 			String imageFileName, String algorithmsCombination, int featureDetector, int descriptorExtractor, int descriptorMatcher)
 	{
 		long startTime = System.nanoTime();
@@ -121,7 +135,7 @@ public class EuroMatch {
 		Highgui.imwrite(RESULT_FOLDER + imageFileName + '.' + algorithmsCombination + ".result.jpg", result.outputImage);
 		long endTime = System.nanoTime();
 		
-		double elapsedTime = (endTime - startTime) / 1000000000.0;
+        result.elapsedTime = (endTime - startTime) / 1000000000.0;
 		
 		String textResultFilename = RESULT_FOLDER + imageFileName + '.' + algorithmsCombination + ".result.txt";
 		try (BufferedWriter writer = new BufferedWriter(new FileWriter(textResultFilename)))
@@ -132,10 +146,11 @@ public class EuroMatch {
 			}
 			writer.write("Matched " + result.detectedBills.size() + " bills" + "\r\n");
 			writer.write("Total Value: " + result.totalValue + "\r\n");
-			writer.write("Total Elapsed Time: " + elapsedTime + "\r\n");
+			writer.write("Total Elapsed Time: " + result.elapsedTime + "\r\n");
 		} catch (IOException e)
 		{
 			e.printStackTrace();
 		}
+        return result;
 	}
 }
